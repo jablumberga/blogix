a48bf0240be4f6f3d4c4676c5edfd75c9ccc7ed0
jablumberga <jablumberga@gmail.com>
2026-04-18 01:54:38 -0400

    Create categories.js

---
+++ src/constants/categories.js
@@ -0,0 +1,6 @@
+export const EXPENSE_CATEGORIES = [
+  "salary", "fuel", "repair", "loan", "maintenance", "toll", "tire",
+  "insurance", "driverPay", "adelanto_conductor", "helper", "broker_commission", "other"
+];
+
+export const PAYMENT_METHODS = ["cash", "transfer", "check", "credit_card", "credit"];
